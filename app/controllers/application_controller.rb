4b01984e3a951b1803af9752f1e1871e615c0e32
Juan Pascual <jp.pascual@rootstrap.com>
2020-12-04 12:42:56 -0300

    Create users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,22 @@
 class ApplicationController < ActionController::API
+  rescue_from ActiveRecord::RecordNotFound,        with: :render_not_found
+  rescue_from ActiveRecord::RecordInvalid,         with: :render_record_invalid
+  rescue_from ActionController::ParameterMissing,  with: :render_parameter_missing
+
+  private
+
+  def render_not_found(exception)
+    logger.info { exception } # for logging
+    render json: { error: I18n.t('api.v1.errors.not_found') }, status: :not_found
+  end
+
+  def render_record_invalid(exception)
+    logger.info { exception } # for logging
+    render json: { errors: exception.record.errors.as_json }, status: :bad_request
+  end
+
+  def render_parameter_missing(exception)
+    logger.info { exception } # for logging
+    render json: { error: I18n.t('api.errors.missing_param') }, status: :unprocessable_entity
+  end
 end
